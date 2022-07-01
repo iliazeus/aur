5d90a632eef6cc43e6e23dcb3dbe3d902a95526e
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2022-07-01 19:14:08 +0300

    yt-dl-album 0.1.0-1

---
+++ yt-dl-album/yt-dl-album.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+USAGE="Usage: $0 <playlist_url> [out_dir]"
+
+if [ "$#" -eq 0 ]
+then
+  echo "$USAGE"
+  exit 1
+fi
+
+PLAYLIST_URL="${1?}"
+OUT_DIR="${2-.}/tmp/$(xxd -ps -l 8 /dev/random)"
+
+yt-dlp --yes-playlist \
+  --format "aac/aac-hi/m4a/bestaudio/best" \
+  --extract-audio --audio-format "m4a" \
+  --embed-metadata \
+  --output "$OUT_DIR/%(id)s.%(ext)s" \
+  "$PLAYLIST_URL"
+
+beet import "$OUT_DIR"
