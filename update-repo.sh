626cb773c48e59b67a13bb272c05dccddb1945bf
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2023-04-16 01:36:02 +0600

    tweak update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -2,11 +2,20 @@
 
 set -e
 
+git checkout -f gh-pages
+git rm -f *.pkg.tar.zst
+git stash
+git checkout -
+
 cp */*.pkg.tar.zst .
 git checkout -f gh-pages
-repo-add --new --prevent-downgrade --verify --sign iliazeus.db.tar.gz *.pkg.tar.zst
-git add iliazeus.* *.pkg.tar.zst
+git stash pop
+
+repo-add --new --prevent-downgrade --verify --sign iliazeus.db.tar.gz *.pkg.tar.zst || true
+git add --ignore-errors iliazeus.* *.pkg.tar.zst
+
 git commit --amend -m "update $(date -Is)"
 git push --force
-rm *.pkg.tar.zst
+
+rm -f *.pkg.tar.zst
 git checkout -
