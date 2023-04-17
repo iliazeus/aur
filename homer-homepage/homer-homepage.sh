a5da7a1871588907fdfba8f406624944e52b3c20
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2023-04-17 14:11:26 +0600

    homer-homepage 23.02.2-1

---
+++ homer-homepage/homer-homepage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -xE
+
+cachedir="${XDG_CONFIG_HOME:-"$HOME/.cache"}/homer-homepage"
+configdir="${XDG_CONFIG_HOME:-"$HOME/.config"}/homer-homepage"
+datadir="${XDG_DATA_HOME:-"$HOME/.local/share"}/homer-homepage"
+runtimedir="${XDG_RUNTIME_DIR:-"/run/user/$UID"}/homer-homepage"
+
+mkdir -p "$cachedir" "$configdir" "$datadir" "$runtimedir"
+
+mkdir -p "$configdir/assets"
+cp --no-clobber /opt/homer-homepage/assets/config.yml.dist "$configdir/assets/config.yml" || true
+
+mkdir -p "$cachedir/overlay-workdir" "$runtimedir/www"
+fuse-overlayfs -o "lowerdir=$configdir:/opt/homer-homepage,workdir=$cachedir/overlay-workdir" "$runtimedir/www"
+
+darkhttpd "$runtimedir/www" "$@"
+
+umount --quiet "$runtimedir/www" || true
