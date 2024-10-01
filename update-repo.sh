572d6c88ff768c64f0cba8741eb6cbb19908062e
Ilia Pozdnyakov <iliazeus@proton.me>
2024-10-01 23:15:18 +0500

    serve packages from master branch

--- update-repo.sh
+++ update-repo.sh
@@ -5,21 +5,15 @@ packages=(
   dump-projector
   http-prompt-pex
   iso2god
-  # punkomatic2-ruffle
+  punkomatic2-ruffle
   tailscale-systray-git-iliazeus
   yt-dl-album
 )
 
-if [ -e packages.tar ]; then
-  bsdtar -xf packages.tar
-fi
-
 for package in "${packages[@]}"; do
-  package_files="$(makepkg $MAKEPKG_OPTS --dir "$package" --packagelist)"
+  package_files="$(makepkg ${MAKEPKG_OPTS-} --dir "$package" --packagelist)"
   if xargs -n1 test -e <<< "$package_files"; then continue; fi
   rm -f "$package"/*.pkg.*
-  makepkg $MAKEPKG_OPTS -sc --dir "$package"
-  repo-add packages.db.tar $package_files
+  makepkg ${MAKEPKG_OPTS-} -sc --dir "$package"
+  repo-add iliazeus.db.tar $package_files
 done
-
-bsdtar -cf packages.tar */*.pkg.*
