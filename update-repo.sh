795e4da4a4cbfda1288573556bfce6d1c745d88f
Ilia Pozdnyakov <iliazeus@proton.me>
2024-10-02 13:10:54 +0500

    deploy to pages from separate branch

--- update-repo.sh
+++ update-repo.sh
@@ -10,10 +10,19 @@ packages=(
   yt-dl-album
 )
 
+git branch -f pages HEAD
+git switch pages
+
 for package in "${packages[@]}"; do
   package_files="$(makepkg ${MAKEPKG_OPTS-} --dir "$package" --packagelist)"
   if xargs -n1 test -e <<< "$package_files"; then continue; fi
   rm -f "$package"/*.pkg.*
   makepkg ${MAKEPKG_OPTS-} -sc --dir "$package"
   repo-add iliazeus.db.tar $package_files
+  git add -f $package_files
 done
+
+git add -f iliazeus.db* iliazeus.files*
+git commit -m $(date -Id)
+git switch -
+git push -f origin pages
